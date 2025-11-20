f43ed1484d353c173e461370fb20b8912e70b70f
Rodrigo de sousa barbosa <detonadorrodrigo11@gmail.com>
2025-11-20 08:57:15 -0300

    remove redundancy

--- core/settings.py
+++ core/settings.py
@@ -18,11 +18,7 @@ from urllib.parse import urlparse, parse_qsl
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "default-para-desenvolvimento")
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -155,23 +151,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+     ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# settings.py
-# settings.py
-import os
-from pathlib import Path
 
-# ... (assumindo que BASE_DIR está definido corretamente)
-
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_DIRS = [
-   
-    os.path.join(BASE_DIR, 'core', 'static'), 
-]
